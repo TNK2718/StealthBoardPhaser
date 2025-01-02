0282ac813a265a0562968126458953ab62e63f3f
TNK2718 <mizuki2718@ruri.waseda.jp>
2025-01-02 17:42:10 +0900

    cardgame tutorial

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,28 +1,101 @@
-import { Scene } from 'phaser';
+import io from 'socket.io-client';
+import Card from '../helpers/card';
+import Dealer from "../helpers/dealer";
+import Zone from '../helpers/zone';
 
-export class Game extends Scene
-{
-    constructor ()
-    {
-        super('Game');
+export default class Game extends Phaser.Scene {
+    constructor() {
+        super({
+            key: 'Game'
+        });
     }
 
-    create ()
-    {
-        this.cameras.main.setBackgroundColor(0x00ff00);
+    preload() {
+        this.load.image('cyanCardFront', 'assets/CyanCardFront.png');
+        this.load.image('cyanCardBack', 'assets/CyanCardBack.png');
+        this.load.image('magentaCardFront', 'assets/magentaCardFront.png');
+        this.load.image('magentaCardBack', 'assets/magentaCardBack.png');
+    }
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+    create() {
+        this.isPlayerA = false;
+        this.opponentCards = [];
 
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+        this.zone = new Zone(this);
+        this.dropZone = this.zone.renderZone();
+        this.outline = this.zone.renderOutline(this.dropZone);
 
-        this.input.once('pointerdown', () => {
+        this.dealer = new Dealer(this);
 
-            this.scene.start('GameOver');
+        let self = this;
 
+        this.socket = io('http://localhost:3000');
+
+        this.socket.on('connect', function () {
+            console.log('Connected!');
         });
+
+        this.socket.on('isPlayerA', function () {
+            self.isPlayerA = true;
+        })
+
+        this.socket.on('dealCards', function () {
+            self.dealer.dealCards();
+            self.dealText.disableInteractive();
+        })
+
+        this.socket.on('cardPlayed', function (gameObject, isPlayerA) {
+            if (isPlayerA !== self.isPlayerA) {
+                let sprite = gameObject.textureKey;
+                self.opponentCards.shift().destroy();
+                self.dropZone.data.values.cards++;
+                let card = new Card(self);
+                card.render(((self.dropZone.x - 350) + (self.dropZone.data.values.cards * 50)), (self.dropZone.y), sprite).disableInteractive();
+            }
+        })
+
+        this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
+
+        this.dealText.on('pointerdown', function () {
+            self.socket.emit("dealCards");
+        })
+
+        this.dealText.on('pointerover', function () {
+            self.dealText.setColor('#ff69b4');
+        })
+
+        this.dealText.on('pointerout', function () {
+            self.dealText.setColor('#00ffff');
+        })
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+        })
+
+        this.input.on('dragstart', function (pointer, gameObject) {
+            gameObject.setTint(0xff69b4);
+            self.children.bringToTop(gameObject);
+        })
+
+        this.input.on('dragend', function (pointer, gameObject, dropped) {
+            gameObject.setTint();
+            if (!dropped) {
+                gameObject.x = gameObject.input.dragStartX;
+                gameObject.y = gameObject.input.dragStartY;
+            }
+        })
+
+        this.input.on('drop', function (pointer, gameObject, dropZone) {
+            dropZone.data.values.cards++;
+            gameObject.x = (dropZone.x - 350) + (dropZone.data.values.cards * 50);
+            gameObject.y = dropZone.y;
+            gameObject.disableInteractive();
+            self.socket.emit('cardPlayed', gameObject, self.isPlayerA);
+        })
+    }
+
+    update() {
+
     }
 }
